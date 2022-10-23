71ce092cefbafb5cb96094c7ab21ff27a876a5a2
Alexey <alexh2@yandex.ru>
2022-10-23 18:30:43 +0300

    hw9

---
+++ youtube_downloader.py
@@ -0,0 +1,15 @@
+import youtube_dl
+
+ydl_opts = {}
+
+def dwl_vid():
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		ydl.download([zxt])
+
+channel = 1
+while (channel == int(1)):
+	link_of_the_video = input("Скопируйте и вставьте URL видео с YouTube для скачивания: ")
+	zxt = link_of_the_video.strip()
+
+	dwl_vid()
+	channel = int(input("Нажмите 1, если хотите скачать еще видео \nНажмите 0 для выхода "))
